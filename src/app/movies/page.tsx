7ed8ba697b7fabfa2aa12bab022b6c74c63307c8
DKimani <kimanidan585@gmail.com>
2026-07-15 05:25:47 +0300

    Update files

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import getBaseUrl from '@/lib/getBaseUrl';
 import Link from 'next/link';
