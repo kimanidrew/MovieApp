babf587ee0f987f44dc1bc0338afaf5d85ebdc5e
DKimani <kimanidan585@gmail.com>
2026-07-20 02:07:03 +0300

    Update files

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -70,7 +70,7 @@ export default function CategoryFilter({
 
   border: 0px solid rgba(255,255,255,.04);
 
-  border-radius: 0px;
+  border-radius: 20px;
 
   /* Ambient shadow on ALL SIDES */
   box-shadow:
